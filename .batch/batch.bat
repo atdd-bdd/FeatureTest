bea215142dd2b6a6c32842d80ec3ddd1a11dec98
Ken <kpughmisc@pughkilleen.com>
2020-12-03 11:57:18 -0500

    New Stuff

--- .batch/batch.bat
+++ .batch/batch.bat
@@ -1,4 +1,4 @@
-echo > batch.out
-erase FILENAME >> batch.out
-path=GITPATH
-git rm PATHNAME >> batch.out
+echo 1> BATCHOUT 
+cd ROOTFOLDER 2>>BATCHOUT
+path "c:\program files\git\bin"
+git rm PATHNAME 1>> BATCHOUT 2>>&1
